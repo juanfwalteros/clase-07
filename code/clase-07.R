633a01cd49a5df22ae65436faf61230369f1dc2a
juanfwalteros <juanfrawalteros@gmail.com>
2024-03-06 21:28:14 -0500

    Update clase-07.R

--- code/clase-07.R
+++ code/clase-07.R
@@ -18,8 +18,55 @@ p_load(tidyverse, ## manipular/limpiar conjuntos de datos.
 
 ### Veamos un ejemplo:
 df = as_tibble(x = women)
+db <- as.data.frame(mtcars)
+
+#seleccionar usando posicion
+db2 <- db[,1:3]
+
+db2 <- db[,c("mpg","vs")]
+
+
+#seleccionar usando select
+db2 <- select(.data = db,mpg,vs,carb)
+
+#seleccionar usando select (eliminar una variable)
+db2 <- select(.data = db,-mpg)
+
+#seleccionar usando select (mantener grupo de variables)
+db2 <- select(.data =db,mpg:vs)
+
+#seleccionar usando select (mantener grupo de variables por nombre, variables con c)
+colnames(db) #veo los nombres de las variables
+
+starts_with <- dplyr::starts_with
+db2 <- select(db,starts_with("c"))
+db2 <- select(db,-ends_with("t"))
+db2 <- select(db,contains("carb"))
+
+
+## filtrar obsevaciones
+# una condicion
+db2 <- dplyr::filter(.data = db, vs==1) #db2 ahora tiene filtrada
+tabyl(db$vs) #saco cuantos usan motor en v
+db2 <- dplyr::filter(.data = db, vs==1 & am==1)
+
 
 ## Otra forma de hacerlo es emplear el operador pipe `%>%`:  
+
+db2 <- mutate(.data=db, aleatorio=rnorm(nrow(db)))
+db2 <- select(.data=db2, vs, am,aleatorio)
+
+select <- dplyr::select
+
+db2 <- filter(.data=db2, vs==1 & am==1)
+
+#ejemplo dplyr (pipe es %>% o |>)
+db3 <- mutate(.data=db, aleatorio=rnorm(nrow(db))) |> 
+  select(vs,am,aleatorio) %>% filter(vs==1 & am==1)
+
+#bono
+db4 <- select(.data=db2, vs, am,aleatorio) |> 
+  filter(vs==1 & am==1) %>% mutate(aleatorio=rnorm(nrow(db2)))
 
 
 ## Con `%>%` no es necesario mencionar el objeto en cada nueva transformación. Además, las líneas de código se redujeron a la mitad.
@@ -52,6 +99,12 @@ obs_2 = tibble(id = 106:107 ,
 
 
 ## Combinar el conjunto de datos (bind_rows): 
+
+obs <- bind_rows(obs_1,obs_2) #pega usando el nombre de las variables
+
+obs_1 <- obs_1 |> select(age,id,height) #ejemplo reordenando nombres
+obs <- bind_rows(obs_1,obs_2) #no importa si las columnas estan en desorden
+
 
 ### **2.2 Adicionar variables a un conjunto de datos**
 db_1 <- tibble(id = 102:105 , income = runif(4,1000,2000) %>% round())
